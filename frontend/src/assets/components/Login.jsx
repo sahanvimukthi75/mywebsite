a578b219813b5d2ab6c5760886822882a1d4abcf
sahan <sahanvimukthi785@gmail.com>
2025-08-12 00:14:28 +0530

    commit4

--- frontend/src/assets/components/Login.jsx
+++ frontend/src/assets/components/Login.jsx
@@ -12,7 +12,7 @@ function Login ()  {
   const handleSubmit= (e)=>{
   
     e.preventDefault()
-    axios.post('http://localhost:3002/login',{name,password})
+    axios.post('http://192.168.43.90/login',{name,password})
     .then (result=>{
       console.log(result)
       if(result.data ==="success"){
