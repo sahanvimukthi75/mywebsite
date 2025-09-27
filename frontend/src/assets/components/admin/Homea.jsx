dd954f0fc5aa10c5212c572cba5871f247042654
sahanvimukthi75 <137777547+sahanvimukthi75@users.noreply.github.com>
2025-09-28 00:43:50 +0530

    Update Homea.jsx

--- frontend/src/assets/components/admin/Homea.jsx
+++ frontend/src/assets/components/admin/Homea.jsx
@@ -16,7 +16,7 @@ const Homea = () => {
 
   useEffect(() => {
     if (id) {
-      axios.get(`/api/gallery/${id}`)
+      axios.get(`http://localhost:5000/gallery/${id}`)
         .then(result => {
           const post = result.data;
           setFormData({
@@ -67,14 +67,14 @@ const Homea = () => {
     });
 
     if (id) {
-      axios.put(`/api/update/${id}`, data)
+      axios.put(`http://localhost:5000/update/${id}`, data)
         .then(res => {
           console.log('Updated:', res.data);
           navigate('/details');
         })
         .catch(err => console.log(err));
     } else {
-      axios.post('/api/addpost', data)
+      axios.post('http://localhost:5000/addpost', data)
         .then(res => {
           console.log('Added:', res.data);
           navigate('/details');
