7969bd1513c972c186ebddd120a8185959d5e5cd
sahanvimukthi75 <137777547+sahanvimukthi75@users.noreply.github.com>
2025-10-03 13:59:45 +0530

    Update Gallery.jsx

--- frontend/src/assets/components/user/Gallery.jsx
+++ frontend/src/assets/components/user/Gallery.jsx
@@ -8,7 +8,7 @@ function Gallery() {
   const [posts, setPosts] = useState([]);
 
   useEffect(() => {
-    axios.get('http://localhost:5000/gallery')
+    axios.get('http://54.89.169.194:5000/gallery')
       .then(result => setPosts(result.data))
       .catch(err => console.log(err));
   }, []);
